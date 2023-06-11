4063db8545a3674489828d9eb76dade486e9e68d
AGsilverAG <silverlewis@gmail.com>
2023-06-11 14:11:04 -0400

    fixed hearing UI

--- 2dexample/Clicking.gd
+++ 2dexample/Clicking.gd
@@ -6,8 +6,8 @@ export var unclicked: Texture;
 var isClicking = false;
 
 func _process(_delta):
-	if(isClicking !=  Input.is_action_pressed("ui_select") ):
-		isClicking = Input.is_action_pressed("ui_select") 
+	if(isClicking !=  Input.is_action_pressed("ui_accept") ):
+		isClicking = Input.is_action_pressed("ui_accept") 
 		if(isClicking):
 			self.texture = clicked;
 		else:
--- 2dexample/onButton.gd
+++ 2dexample/onButton.gd
@@ -1,25 +1,34 @@
 extends Node2D
 
 signal onClicked;
-
-
+export var showClicked: bool =true;
 var entered: bool  = false;
 var sent:bool = false
 export var id: int = 0;
 
 func _on_Area2D_body_entered(body):
-	if(body.name == "Player"):
-		print("entered")
+	if(body.name == "Player"||body.name == "Mouse"):
+		print("Entered")
 		entered = true;
 	
 func _on_Area2D_body_exited(body):
-	if(body.name == "Player"):
+	if(body.name == "Player"||body.name == "Mouse"):
 		print("exited")
 		entered = false;
 		sent = false
 	
 func _process(_delta):
-	if(Input.is_action_just_pressed("ui_accept") && entered && !sent):
-		print("sending")
-		sent=true
-		emit_signal("onClicked",id);
+	var node = get_node("Area2D/UiPanel")
+	if(Input.is_action_just_pressed("ui_accept") && entered):
+		print("clicked")
+		if(node&&showClicked):
+			node.modulate = Color(.5,.5,.5)
+		if(!sent):
+			print("sending")
+			sent=true
+			emit_signal("onClicked",id);
+	elif(node&&showClicked&&!Input.is_action_pressed("ui_accept")):
+		node.modulate = Color(1,1,1)
+
+func selected(vis):
+	get_node("Area2D/ColorRect").visible = vis
